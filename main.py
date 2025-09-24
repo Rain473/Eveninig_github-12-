2909fb13131597995cc3c1029cea85e0ef6c45f3
Rain473 <athwinvideshm31@gmail.com>
2025-09-24 21:06:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ name=str(input("Enter your name: "))
 age=int(input("Enter your age: "))
 gender=str(input("Enter your gender: "))
 {}
-print("Hello, "+name+". You are "+str(age)+" years old and your gender is "+gender+".")
+print("Hello, "+name+". You are "+str(age)+" years old and your gender is "+gender+". best of luck")
