dc35a9e6810878e1092f985bbf2a9277d133ff84
Rain473 <athwinvideshm31@gmail.com>
2025-09-24 22:27:43 +0530

    added files

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-name=str(input("Enter your name: "))
-age=int(input("Enter your age: "))
-gender=str(input("Enter your gender: "))
-{}
-print("Hello, "+name+". You are "+str(age)+" years old and your gender is "+gender+". best of luck")
- 
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+gender = input("Enter your gender: ")
+
+print("Hello, " + name + ". You are " + str(age) + " years old and your gender is " + gender + ". Best of luck!")
+print("ok")
